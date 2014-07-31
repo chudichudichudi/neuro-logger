9d43657c5c8af04b09da28e5b51af7f276c195b1
chudi <elchudi@gmail.com>
2014-07-31 19:00:57 -0300

    better api

--- log.py
+++ log.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.admin import Admin
@@ -8,7 +10,8 @@ from marshmallow import Serializer
 
 import json
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='', static_folder='')
+app.debug = False
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
@@ -20,18 +23,18 @@ class Experiment(db.Model):
     experiment_log = db.Column(db.String)
     experiment_name = db.Column(db.String)
 
-    # def __init__(self, test_subject, experiment_log, experiment_name):
-    #     self.test_subject = test_subject
-    #     self.experiment_log = experiment_log
-    #     self.experiment_name = experiment_name
-
 
 class ExperimentSerializer(Serializer):
     class Meta:
         fields = ('id', 'test_subject', 'experiment_log', "experiment_name")
 
+
+class ExperimentAdminView(ModelView):
+    column_list = ('id', 'test_subject', 'experiment_log', 'experiment_name')
+
+
 admin = Admin(app)
-admin.add_view(ModelView(Experiment, db.session))
+admin.add_view(ExperimentAdminView(Experiment, db.session))
 
 
 @app.route('/')
@@ -42,12 +45,16 @@ def default():
 @app.route('/create_log', methods=['POST'])
 @cross_origin()
 def create_log():
-    if not request.json:
-        return "error not json", 400
+    if not request.json or not 'test_subject' in request.json \
+            or not 'experiment_name' in request.json:
+        return ("error, missing parameter "
+                "(test_subject, experiment_name) or not json"), 400
     test_subject = request.json['test_subject'] or 'nn'
-    experiment_log = json.dumps(request.json['experiment_log']) or ''
-    experiment_name = json.dumps(request.json['experiment_name']) or ''
-    experiment = Experiment(test_subject, experiment_log, experiment_name)
+    experiment_name = request.json['experiment_name'] or ''
+    experiment = Experiment()
+    experiment.test_subject = test_subject
+    experiment.experiment_name = experiment_name
+    experiment.experiment_log = "[]"
     db.session.add(experiment)
     db.session.commit()
     return str(experiment.id), 201
@@ -57,7 +64,8 @@ def create_log():
 @cross_origin()
 def get_experiment(experiment):
     experiments = db.session.query(Experiment)\
-        .filter(Experiment.experiment_name == experiment)
+        .filter(Experiment.experiment_name
+                == experiment)
     return jsonify({"experiments":
                     ExperimentSerializer(experiments, many=True).data}), 200
 
@@ -73,16 +81,19 @@ def get_log(id):
 @cross_origin()
 def append_log():
     if not request.json or not 'id' in request.json \
-            or not 'log' in request.json:
+            or not 'experiment_log' in request.json:
         return "error, missing parameter (id, log) or not json", 400
     experiment = Experiment.query.get(request.json['id'])
-    new_log = json.loads(experiment.log) or ''
-    new_log.append(json.loads(request.json['log']))
-    experiment.experiment_log = new_log
+    new_log = json.loads(experiment.experiment_log)
+    new_log.append(request.get_json()['experiment_log'])
+    experiment.experiment_log = json.dumps(new_log)
     db.session.commit()
     return "ok", 200
 
 if __name__ == '__main__':
     app.debug = True
+    app.static_url_path = ''
+    app.static_folder = ("/home/chudi/trabajo/neuro/neuro-metacognition/"
+                         "metacog/compiled")
     app.trap_http_exceptions = True
     app.run()
